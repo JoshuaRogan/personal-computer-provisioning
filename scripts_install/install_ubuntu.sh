3d345595d5545180f24fff341c41cec6a548033e
Josh <jrogan@wikia-inc.com>
2017-02-13 00:48:33 -0800

    Updates

--- scripts_install/install_ubuntu.sh
+++ scripts_install/install_ubuntu.sh
@@ -1,4 +1,40 @@
 #!/bin/bash
+u="-u"
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+packages="\
+git \
+httpie\
+zsh
+vim
+python-pip python-dev build-essential  \
+git-core \
+silversearcher-ag
+"
+
+# sudo apt update
+# #Install
+# sudo apt --ignore-missing install $packages
+#
+# ######################### HTTPSTAT #########################
+# sudo pip install --upgrade pip
+# sudo pip install --upgrade virtualenv
+# sudo pip install httpstat
+# ######################### HTTPSTAT #########################
+#
+# ######################### ZSH OH MY ZSH ########################
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
+#
+#
+# cp $parent_path/.zshrc
+# source ~/.zshrc
+# ######################## ZSH OH MY ZSH #########################
+
+######################### ATOM EDIT #########################
+# curl -o /usr/local/bin/rmate https://raw.githubusercontent.com/aurora/rmate/master/rmate
+# sudo chmod +x /usr/local/bin/rmate
+
+######################### ATOM EDIT #########################
+
+
+echo "DONE!"
