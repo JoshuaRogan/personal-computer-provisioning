ebd44ffc1353ed67aac04ca46663d7b31f5280fc
Josh <jrogan@wikia-inc.com>
2017-02-22 21:26:12 -0800

    update

--- install_scripts/all.sh
+++ install_scripts/all.sh
@@ -5,16 +5,8 @@ source ${INSTALL_ALL_DIR}/../utils/utils.sh
 source ./../utils/utils.sh 2> /dev/null
 
 install_prezto() {
+    cmn_assert_file_does_not_exist "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-}
-
-install_lnav() {
-    local filename=lnav-0.8.1-linux-64bit.zip
-    wget https://github.com/tstack/lnav/releases/download/v0.8.1/$filename
-    unzip $filename
-    rm -f $filename
-    sudo cp lnav-0.8.1/lnav /usr/local/bin/lnav
-    rm -rf lnav-0.8.1
 }
 
 main() {
