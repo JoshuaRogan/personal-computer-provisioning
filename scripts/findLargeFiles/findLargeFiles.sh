7cdb9ead9e493351845a0fc87c95a20bfce5ec43
Josh <jrogan@wikia-inc.com>
2020-06-12 13:25:47 -0400

    Update profile

--- scripts/findLargeFiles/findLargeFiles.sh
+++ scripts/findLargeFiles/findLargeFiles.sh
@@ -2,4 +2,3 @@ outfilename="find-large-files-$(date +%Y%m%d%s).txt"
 echo "Saving to $outfilename"
 touch $outfilename
 sudo find / -type f -size +200000k -exec ls -lh {} \; | awk '{ print $9 ": " $5 }' >> $outfilename
-
